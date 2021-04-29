42256ead1d36cc49b3eacb027f49dfbb048afee6
Vlad Sirenco <vladpolt@gmail.com>
2021-04-29 15:08:05 +0300

    Task4-4

---
+++ JS/Lesson4/Task3/index.js
@@ -0,0 +1,9 @@
+// Таблица умножения
+// eslint-disable-next-line no-plusplus
+for(let i = 1; i<=5; i++){
+    console.log('=>'+i+'<=');
+    for(let j =1; j<=9; j+=1){
+          console.log(i+' x '+j+' = '+i*j)
+    }
+}
+
---
+++ JS/Lesson4/Task4_ESLint/index.js
@@ -0,0 +1,13 @@
+function run() {
+    var a = 0;
+  
+    if (a == 1) {
+      return c;
+    }
+    for (let i = 1; i < 10; i++) {
+      console.log(i);
+    }
+    return (d = a);
+  }
+  
+  run();
