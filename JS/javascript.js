b6d7059df1d741c05f44f3821885cdbf46800bee
Vlad Sirenco <vladpolt@gmail.com>
2021-05-13 13:59:44 +0300

    task1

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,18 +1,16 @@
-const uniqueCount = (array) => {
-  if (!Array.isArray(array)) return null;
-  const newArray = [];
-  for (let i = 0; i < array.length; i += 1) {
-   // let flag = false;
-    for (let j = i + 1; j < array.length; j += 1) {
-      if (array[i] === array[j]) {
-        array[j] = null;
-       // flag = true;
-      }
-      //if (j === array.length - 1 && flag === true) array[i] = null;
-    }
-    if (array[i] !== null) newArray.push(array[i]);
-  }
-  return newArray.length;
-};
+const getMessagesForBestStudents = (allStudentsList, studentsForRetake) =>
+  //put your code here
+  allStudentsList
+    .filter((names) => !studentsForRetake.includes(names))
+    .map((name) => 'Good job, ' + name);
 
-uniqueCount([3, 6, 7, 8, 7, 6, 3, 4, 2, 3, 7, 8, 3]);
+//example 1:
+
+//input:
+const allStud = ['a', 'b', 'c', 'd'];
+const retakeStud = ['b', 'd'];
+
+getMessagesForBestStudents(allStud, retakeStud);
+
+//output:
+//['Good job, a', 'Good job, c']
