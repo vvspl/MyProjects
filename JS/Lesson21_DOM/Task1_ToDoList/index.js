8af45edff068af811b6cef96f0ac1c421253ea79
vvspl <vladpolt@gmail.com>
2021-06-11 17:03:25 +0300

    Task

---
+++ JS/Lesson21_DOM/Task1_ToDoList/index.js
@@ -0,0 +1,31 @@
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const renderListItems = (listItems) => {
+  const baseEl = document.querySelector('.list'); // находим родительский эл-т
+
+  const newListOfEl = listItems
+    .sort((a, b) => a.text - b.text)
+    .map(({ text, done }) => {
+      // перебираем объект с текстом для новых эл-тов
+
+      const newEl = document.createElement('li'); // создаем элемент
+      const newCheckBx = document.createElement('input'); // создаем чекбокс
+      newCheckBx.setAttribute('type', 'checkbox');
+      newCheckBx.checked = done;
+      newCheckBx.classList.add('list__item-checkbox'); // класс для чекбокса
+      if (done) newEl.classList.add('list__item_done'); // если checked - добавляем другой класс
+      newEl.classList.add('list__item'); // добавляем класс для нового элемента
+      newEl.append(newCheckBx, text); // добавляем текст в элемент
+
+      return newEl; // новая строка добавлена в массив
+    });
+
+  baseEl.append(...newListOfEl); // добавляем к начальному элементу массив новых эл-тов
+};
+renderListItems(tasks);
