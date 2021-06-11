386fc220ed47783e7e17ebfa1ade47c558650a63
vvspl <vladpolt@gmail.com>
2021-06-11 18:43:35 +0300

    Task

--- JS/Lesson21_DOM/Task4_textContent/index.js
+++ JS/Lesson21_DOM/Task4_textContent/index.js
@@ -1,22 +1,26 @@
 export const getTitle = () => {
-  const a = document.querySelector('.title');
-  return a.textContent;
-};
-
-export const getDescription = () => {
-  const a = document.querySelector('.about');
-  return a.innerText;
-};
-
-export const getPlans = () => {
-  const a = document.querySelector('.plans');
-  return a.innerHTML;
-};
-
-export const getGoal = () => {
-  const a = document.querySelector('.goal');
-  return a.outerHTML;
-};
+    const titleElement = document.querySelector('.title');
+  
+    return titleElement.textContent;
+  };
+  
+  export const getDescription = () => {
+    const descriptionElement = document.querySelector('.about');
+  
+    return descriptionElement.innerText;
+  };
+  
+  export const getPlans = () => {
+    const plansElement = document.querySelector('.plans');
+  
+    return plansElement.innerHTML;
+  };
+  
+  export const getGoal = () => {
+    const goalElement = document.querySelector('.goal');
+  
+    return goalElement.outerHTML;
+  };
 
 console.log(getTitle());
 console.log(getDescription());
