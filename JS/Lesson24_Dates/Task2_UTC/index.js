f91e01749b85ca88b807989288b154ae456c2788
vvspl <vladpolt@gmail.com>
2021-06-15 20:57:28 +0300

    Task

--- JS/Lesson24_Dates/Task2_UTC/index.js
+++ JS/Lesson24_Dates/Task2_UTC/index.js
@@ -1,10 +1,10 @@
-export const formatter = new Intl.DateTimeFormat('en', {
-    timeZone: 'UTC',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false,
-  });
-  
-  const getGreenwichTime = date => formatter.format(date);
-  
-  console.log(getGreenwichTime(new Date()));
+const formatter = new Intl.DateTimeFormat('en', {
+  timeZone: 'UTC',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+});
+
+export const getGreenwichTime = (date) => formatter.format(date);
+
+console.log(getGreenwichTime(new Date()));
