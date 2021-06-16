92d714aa67006e279bb1552a35b2e61efd53d8db
vvspl <vladpolt@gmail.com>
2021-06-16 19:50:42 +0300

    Task

---
+++ JS/Lesson27_Storage/Task1/index.js
@@ -0,0 +1,16 @@
+localStorage.setItem('person', JSON.stringify({ name: 'Tom' }));
+localStorage.setItem('age', JSON.stringify(17));
+console.log(Object.entries(localStorage));
+const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch {
+      newValue = value;
+    }
+    return { ...acc, [key]: newValue };
+  }, {});
+};
+
+console.log(getLocalStorageData());
