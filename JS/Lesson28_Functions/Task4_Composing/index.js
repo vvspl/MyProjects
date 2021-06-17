f9628f6dc9b5eb39cee836292a02950e3e1ea47f
vvspl <vladpolt@gmail.com>
2021-06-17 17:31:46 +0300

    task

--- JS/Lesson28_Functions/Task4_Composing/index.js
+++ JS/Lesson28_Functions/Task4_Composing/index.js
@@ -2,7 +2,7 @@ const add2 = value => value +2;
 const square = value => value*value;
 const half = value => value/2;
 
-const compose = (...funcs) => value => {
+export const compose = (...funcs) => value => {
     return funcs.reduce((acc, func) => func(acc), value)
 }
 
