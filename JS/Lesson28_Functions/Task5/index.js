629a310f5b8444938fe444a0af0e30fdc70a2705
vvspl <vladpolt@gmail.com>
2021-06-17 20:11:40 +0300

    task

--- JS/Lesson28_Functions/Task5/index.js
+++ JS/Lesson28_Functions/Task5/index.js
@@ -1,27 +1,35 @@
 export const shmoment = (date) => {
-    const dateObj = { 
-        years: +`${date.getFullYear()}`,
-        months: +`${date.getMonth()}`,
-        days: +`${date.getDay()}`,
-        hours: +`${date.getHours()}`,
-        minutes: +`${date.getMinutes()}`,
-        seconds: +`${date.getSeconds()}`,
-        milliseconds: +`${date.getMilliseconds()}`,
-    };
-    const dataCalc = {
-    add(dat, value){
-        dateObj[dat] += value;
-        return this;
+  const dateObj = {
+    years: +`${date.getFullYear()}`,
+    months: +`${date.getMonth()}`,
+    days: +`${date.getDay()}`,
+    hours: +`${date.getHours()}`,
+    minutes: +`${date.getMinutes()}`,
+    seconds: +`${date.getSeconds()}`,
+    milliseconds: +`${date.getMilliseconds()}`,
+  };
+  const dataCalc = {
+    add(dat, value) {
+      dateObj[dat] += value;
+      return this;
     },
-    subtract(dat, value){
-        dateObj[dat] -= +value;
-        return this;
+    subtract(dat, value) {
+      dateObj[dat] -= +value;
+      return this;
     },
-    result(){
-        return dateObj;
-    }
-}
-return dataCalc;
+    result() {
+      const newDateObj = new Date(
+        dateObj.years,
+        dateObj.months,
+        dateObj.hours,
+        dateObj.minutes,
+        dateObj.seconds,
+        dateObj.milliseconds
+      );
+      return newDateObj;
+    },
+  };
+  return dataCalc;
 };
 
 const res = shmoment(new Date(2020, 1, 7, 17, 17, 17))
@@ -29,4 +37,4 @@ const res = shmoment(new Date(2020, 1, 7, 17, 17, 17))
   .add('days', 8)
   .subtract('years', 1)
   .result();
-  console.log(res);
+console.log(res);
