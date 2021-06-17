4dab6764934aa667ae320e98986a3140ebb1c705
vvspl <vladpolt@gmail.com>
2021-06-17 15:57:20 +0300

    task

--- JS/Lesson28_Functions/Task2_Recursion/index.js
+++ JS/Lesson28_Functions/Task2_Recursion/index.js
@@ -23,7 +23,7 @@ const tree = {
   ],
 };
 
-const markFavorites = (tree, favorites) => {
+export const markFavorites = (tree, favorites) => {
   const isFavorite = favorites.includes(tree.id);
   return {
     ...tree,
