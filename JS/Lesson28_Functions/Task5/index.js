088dc05622e7b31bbca007df01a1d8bf46cf4910
vvspl <vladpolt@gmail.com>
2021-06-19 15:04:53 +0300

    task

--- JS/Lesson28_Functions/Task5/index.js
+++ JS/Lesson28_Functions/Task5/index.js
@@ -1,24 +1,47 @@
 export const shmoment = (date) => {
-  const dateObj = {
-    years: +`${365 * 24 * 60 * 60 * 1000}`,
-    months: +`${(365 / 12) * 24 * 60 * 60 * 1000}`,
-    days: +`${24 * 60 * 60 * 1000}`,
-    hours: +`${60 * 60 * 1000}`,
-    minutes: +`${60 * 1000}`,
-    seconds: +`${1000}`,
-    milliseconds: +`${1}`,
+  let newDate = date;
+  const addDate = (dat, value) => {
+    if (dat === 'years')
+      newDate = new Date(new Date(newDate).setFullYear(new Date(newDate).getFullYear() + value));
+    else if (dat === 'months')
+      newDate = new Date(new Date(newDate).setMonth(new Date(newDate).getMonth() + value));
+    else if (dat === 'days')
+      newDate = new Date(new Date(newDate).setDate(new Date(newDate).getDate() + value));
+    else if (dat === 'hours')
+      newDate = new Date(new Date(newDate).setHours(new Date(newDate).getHours() + value));
+    else if (dat === 'minutes')
+      newDate = new Date(new Date(newDate).setMinutes(new Date(newDate).getMinutes() + value));
+    else if (dat === 'seconds')
+      newDate = new Date(new Date(newDate).setSeconds(new Date(newDate).getSeconds() + value));
+    else if (dat === 'millieconds')
+      newDate = new Date(new Date(newDate).setMillieconds(new Date(newDate).getMillieconds() + value));
+    return newDate;
   };
-  let newDate = Date.parse(date);
+  const subtractDate = (dat, value) => {
+    if (dat === 'years')
+      newDate = new Date(new Date(newDate).setFullYear(new Date(newDate).getFullYear() - value));
+    else if (dat === 'months')
+      newDate = new Date(new Date(newDate).setMonth(new Date(newDate).getMonth() - value));
+    else if (dat === 'days')
+      newDate = new Date(new Date(newDate).setDate(new Date(newDate).getDate() - value));
+    else if (dat === 'hours')
+      newDate = new Date(new Date(newDate).setHours(new Date(newDate).getHours() - value));
+    else if (dat === 'minutes')
+      newDate = new Date(new Date(newDate).setMinutes(new Date(newDate).getMinutes() - value));
+    else if (dat === 'seconds')
+      newDate = new Date(new Date(newDate).setSeconds(new Date(newDate).getSeconds() - value));
+    else if (dat === 'millieconds')
+      newDate = new Date(new Date(newDate).setMillieconds(new Date(newDate).getMillieconds() - value));
+    return newDate;
+  };
 
   const dataCalc = {
     add(dat, value) {
-      newDate += dateObj[dat] * value;
-      console.log(new Date(newDate));
+      addDate(dat, value);
       return this;
     },
     subtract(dat, value) {
-      newDate -= dateObj[dat] * value;
-      console.log(new Date(newDate));
+      subtractDate(dat, value);
       return this;
     },
     result() {
@@ -28,15 +51,37 @@ export const shmoment = (date) => {
   return dataCalc;
 };
 
-const res = shmoment(new Date(2020, 1, 7, 17, 17, 17))
+// const res = shmoment(new Date(2020, 1, 7, 17, 17, 17))
+//   .add('minutes', 2)
+//   .add('days', 8)
+//   .add('years', 8)
+//   .add('months', 8)
+//   .subtract('years', 5)
+//   .subtract('years', 1)
+//   .subtract('hours', 2)
+//   .subtract('seconds', 20)
+//   .subtract('milliseconds', 200)
+//   .result();
+// console.log(res);
+
+const res = shmoment(new Date(Date.UTC(2020, 0, 1)))
+  .add('years', 2)
+  .add('months', 8)
   .add('minutes', 2)
   .add('days', 8)
   .add('years', 8)
   .add('months', 8)
   .subtract('years', 5)
-  .subtract('years', 1)
+  .subtract('years', 3)
   .subtract('hours', 2)
   .subtract('seconds', 20)
   .subtract('milliseconds', 200)
   .result();
 console.log(res);
+
+
+
+// function year() {
+//   let Dy = new Date(1999,11,31);
+//   Dy.setFullYear(Dy.getFullYear() + 3);
+// }
