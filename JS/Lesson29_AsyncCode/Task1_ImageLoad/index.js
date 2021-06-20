a3ade51e6c81ebbf0516e4308690fe70dd7ea678
vvspl <vladpolt@gmail.com>
2021-06-20 21:19:59 +0300

    task

--- JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
+++ JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
@@ -1,29 +1,30 @@
 export const addImage = (imgSrc, callback) => {
-  const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'My photo');
-  imgElem.src = imgSrc;
-  const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
-
-  const onImageLoaded = () => {
-    const { width, height } = imgElem;
-    callback(null, { width, height });
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+  
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      callback(null, { width, height });
+    };
+  
+    imgElem.addEventListener('load', onImageLoaded);
+  
+    imgElem.addEventListener('error', () => callback('Image load is failed...'));
   };
-
-  imgElem.addEventListener('load', onImageLoaded);
-
-  imgElem.addEventListener('error', () => callback('Image load is failed...'));
-};
-
-const onImageLoaded = (error, data) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const { width, height } = data;
-  const sizeElem = document.querySelector('.image-size');
-  sizeElem.textContent = `${width} x ${height}`;
-};
-
-// const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
-// addImage(imgSrc, onImageLoaded);
+  
+  const onImageLoaded = (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const { width, height } = data;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} x ${height}`;
+  };
+  
+  // const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+  // addImage(imgSrc, onImageLoaded);
+  
