aa2e5460c01b5b4f016f5ac298b946d74f839d69
vvspl <vladpolt@gmail.com>
2021-06-20 21:00:45 +0300

    task

--- JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
+++ JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
@@ -17,14 +17,14 @@ export const addImage = (imgSrc, callback) => {
 
 const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
 
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-// addImage(imgSrc, onImageLoaded);
+addImage(imgSrc, onImageLoaded);
