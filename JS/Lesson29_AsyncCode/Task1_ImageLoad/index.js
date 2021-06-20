e4326760734d46ebcf8be02dbf5912b040d12620
vvspl <vladpolt@gmail.com>
2021-06-20 21:38:16 +0300

    task

--- JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
+++ JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
@@ -1,28 +1,25 @@
 export const addImage = (imgSrc, callback) => {
-    const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt', 'My Photo');
-    imgElem.src = imgSrc;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
-  
-    imgElem.addEventListener('load', () => callback(null, imgElem));
-    imgElem.addEventListener('error', () => callback('Image load is failed...'));
-  };
-  
-  // const imgSrc =
-  //   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-  
-  const onImageLoaded = (error, imgElem) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    const { width, height } = imgElem;
-    const sizeElem = document.querySelector('.image-size');
-    sizeElem.textContent = `${width} x ${height}`;
-  };
-  
-  // addImage(
-  //   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  //   onImageLoaded
-  // );
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  imgElem.addEventListener('load', () => callback(null, imgElem));
+  imgElem.addEventListener('error', () => callback('Image load is failed...'));
+};
+
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+addImage(
+  'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg',
+  onImageLoaded
+);
