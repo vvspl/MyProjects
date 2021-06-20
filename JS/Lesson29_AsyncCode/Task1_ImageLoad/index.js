bcbe3dd2610351d96210669ae23927e401ba7cce
vvspl <vladpolt@gmail.com>
2021-06-20 20:49:52 +0300

    task

---
+++ JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
@@ -0,0 +1,30 @@
+export const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener('load', onImageLoaded);
+
+  imgElem.addEventListener('error', () => callback('Image load is failed...'));
+};
+
+const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+addImage(imgSrc, onImageLoaded);
