6eed12b44e7d49dc607b89b3064fe5bdb1264141
vvspl <vladpolt@gmail.com>
2021-06-20 21:28:22 +0300

    task

--- JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
+++ JS/Lesson29_AsyncCode/Task1_ImageLoad/index.js
@@ -1,30 +1,33 @@
 export const addImage = (imgSrc, callback) => {
-    const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt', 'My photo');
-    imgElem.src = imgSrc;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
-  
-    const onImageLoaded = () => {
-      const { width, height } = imgElem;
-      callback(null, { width, height });
-    };
-  
-    imgElem.addEventListener('load', onImageLoaded);
-  
-    imgElem.addEventListener('error', () => callback('Image load is failed...'));
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
   };
-  
-  const onImageLoaded = (error, data) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    const { width, height } = data;
-    const sizeElem = document.querySelector('.image-size');
-    sizeElem.textContent = `${width} x ${height}`;
-  };
-  
-  // const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
-  // addImage(imgSrc, onImageLoaded);
-  
+
+  imgElem.addEventListener('load', onImageLoaded);
+
+  const onError = () => callback('Image load is failed...');
+
+  imgElem.addEventListener('error', onError);
+};
+
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded
+);
