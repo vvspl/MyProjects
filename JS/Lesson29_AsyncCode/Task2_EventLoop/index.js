299f91b6cd3aff4cf9aa64c5659259f54c31ed80
vvspl <vladpolt@gmail.com>
2021-06-20 22:38:28 +0300

    task

---
+++ JS/Lesson29_AsyncCode/Task2_EventLoop/index.js
@@ -0,0 +1,13 @@
+const pinger = (num, period) => {
+    let i = num;
+    console.log('Ping');
+    const interval = setInterval(() => {
+      if (--i > 0) {
+        console.log('Ping');
+      } else {
+        clearInterval(interval);
+      }
+    }, period);
+  };
+  
+  pinger(5, 5000);
