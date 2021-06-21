fba332e98ffd06a2ec9799f7c47affc9dae90977
vvspl <vladpolt@gmail.com>
2021-06-21 12:05:11 +0300

    task

--- JS/Lesson30_Promises/Task1_ImgLoad/index.js
+++ JS/Lesson30_Promises/Task1_ImgLoad/index.js
@@ -1,30 +1,29 @@
-// export 
-const addImage = imgSrc => {
-    const p = new Promise((resolve, reject) => {
-      const imgElem = document.createElement('img');
-      imgElem.setAttribute('alt', 'My photo');
-      imgElem.src = imgSrc;
-      const containerElem = document.querySelector('.page');
-      containerElem.append(imgElem);
-  
-      const onImageLoaded = () => {
-        const { width, height } = imgElem;
-        resolve({ width, height });
-      };
-  
-      imgElem.addEventListener('load', onImageLoaded);
-  
-      const onError = () => reject(new Error('Image load is failed...'));
-  
-      imgElem.addEventListener('error', onError);
-    });
-  
-    return p;
-  };
-  
-  const imgSrc =
+export const addImage = (imgSrc) => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    const onError = () => reject(new Error('Image load is failed...'));
+
+    imgElem.addEventListener('error', onError);
+  });
+
+  return p;
+};
+
+const imgSrc =
   'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
-  
-  const res = addImage(imgSrc);
-  console.log(res);
-  res.then(data=>console.log(data));
+
+const res = addImage(imgSrc);
+console.log(res);
+res.then((data) => console.log(data));
