80fecfca107d8d7860c0be6d3069a83ac4b7f36b
vvspl <vladpolt@gmail.com>
2021-06-29 15:20:20 +0300

    task

--- JS/Lesson34_HttpRequests/Task3_CreateForm/index.js
+++ JS/Lesson34_HttpRequests/Task3_CreateForm/index.js
@@ -43,7 +43,8 @@ const onSubmit = (event) => {
     body: JSON.stringify(taskData),
   })
     .then((response) => {
-      alert(response.json()); // вывод ответа сервера
+      alert(response.body); // вывод ответа сервера
+      console.log(response);
             if (response.status === 201) mainForm.reset();
     })
     .catch(() => {
