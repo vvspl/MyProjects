887bd127af7bf648d9f666ecf2233ae020d172d4
vvspl <vladpolt@gmail.com>
2021-06-29 14:31:58 +0300

    task

--- JS/Lesson34_HttpRequests/Task3_CreateForm/index.js
+++ JS/Lesson34_HttpRequests/Task3_CreateForm/index.js
@@ -2,21 +2,23 @@ const inputs = document.querySelector('.login-form');
 const submitBtn = document.querySelector('.submit-button');
 const errorTxt = document.querySelector('.error-text');
 
- // проверка на валидацию всех полей
+// проверка на валидацию всех полей
 inputs.addEventListener('change', () => {
-  if (inputs.reportValidity()) submitBtn.removeAttribute('disabled');
+  if (inputs.reportValidity()) {
+    submitBtn.removeAttribute('disabled');
+    submitBtn.enabled = true;
+  }
 });
 
 // получение данных с формы для отправки на сервер
 const mainForm = document.querySelector('.login-form');
 
-
 // отправка данных на сервер
 const baseUrl = 'https://60d8828beec56d0017477359.mockapi.io/api/v1/users';
 
 const onSubmit = (event) => {
-    event.preventDefault();
-  const taskData = Object.fromEntries(new FormData(mainForm))
+  event.preventDefault();
+  const taskData = Object.fromEntries(new FormData(mainForm));
   console.log(taskData);
   return fetch(baseUrl, {
     method: 'POST',
@@ -25,17 +27,13 @@ const onSubmit = (event) => {
     },
     body: JSON.stringify(taskData),
   })
-  .then((response) => {
-    alert(response)  // вывод ответа сервера
-    if (response.status===201) mainForm.reset();
-    console.log(response);
-  })
-  .catch(() => {
-    errorTxt.textContent = 'Failed to create user';
-  });
-  ; 
-  
+    .then((response) => {
+      alert(JSON.stringify(response)); // вывод ответа сервера
+      if (response.status === 201) mainForm.reset();
+    })
+    .catch(() => {
+      errorTxt.textContent = 'Failed to create user';
+    });
 };
 
 submitBtn.addEventListener('click', onSubmit);
-
