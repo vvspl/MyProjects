7ee8e6a2f5416bf62e953d60f15944a50a627aa6
vvspl <vladpolt@gmail.com>
2021-06-29 22:32:23 +0300

    task

---
+++ JS/Lesson35_Errors/Task4/index.js
@@ -0,0 +1,10 @@
+const parseUser = str => {
+    try {
+      return JSON.parse(str);
+    } catch (error) {
+      return null;
+    }
+  };
+  
+  console.log(parseUser('{"name": "IronMan"}'));
+  console.log(parseUser('some text'));
