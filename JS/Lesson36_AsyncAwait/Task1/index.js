03a47fff81d34a7f3cb376354a7d52eab475026a
vvspl <vladpolt@gmail.com>
2021-06-30 15:49:05 +0300

    task

--- JS/Lesson36_AsyncAwait/Task1/index.js
+++ JS/Lesson36_AsyncAwait/Task1/index.js
@@ -1,4 +1,4 @@
-const fetchUser = async userId => {
+export const fetchUser = async userId => {
     try {
       const response = await fetch(`https://api.github.com/users/${userId}`);
       if (!response.ok) {
