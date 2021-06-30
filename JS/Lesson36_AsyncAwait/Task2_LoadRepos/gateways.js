5f2ae537cd14c6b3abff85e7fd28960d18c66307
vvspl <vladpolt@gmail.com>
2021-06-30 15:59:16 +0300

    task

---
+++ JS/Lesson36_AsyncAwait/Task2_LoadRepos/gateways.js
@@ -0,0 +1,21 @@
+export const fetchUserData = async (userName) => {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+
+  if (response.ok) {
+    const result = await response.json();
+    return result;
+  }
+
+  throw new Error('Failed to load data');
+};
+
+export const fetchRepositories = async (url) => {
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const result = await response.json();
+    return result;
+  }
+
+  throw new Error('Failed to load data');
+};
