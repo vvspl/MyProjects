6c619590cbbb75a7654d22549cfb48ccf95160bf
vvspl <vladpolt@gmail.com>
2021-06-30 15:46:25 +0300

    task

---
+++ JS/Lesson36_AsyncAwait/Task1/index.js
@@ -0,0 +1,12 @@
+const fetchUser = async userId => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${userId}`);
+      if (!response.ok) {
+        return null;
+      }
+      const userData = await response.json();
+      return userData;
+    } catch (err) {
+      throw new Error('Failed to fetch user');
+    }
+  };
