4122832d81c7673df9b740c554948ca6bcd63f54
vvspl <vladpolt@gmail.com>
2021-07-28 18:25:00 +0300

    task

--- LiveSessions/React/Session3_Routers/src/User.jsx
+++ LiveSessions/React/Session3_Routers/src/User.jsx
@@ -1,20 +1,59 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-debugger */
+/* eslint-disable camelcase */
+/* eslint-disable class-methods-use-this */
 /* eslint-disable arrow-body-style */
 import React from 'react';
 
-const User = () => {
-  return (
-    <div className="user">
-      <img
-        alt="User Avatar"
-        src="https://avatars1.githubusercontent.com/u/9919?v=4"
-        className="user__avatar"
-      />
-      <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+class User extends React.Component {
+  state = {
+    userName: null,
+    userLocation: null,
+    userAvatar: null,
+  };
+
+  componentDidMount() {
+    // debugger;
+    const userId = this.props.match.params.userId;
+    console.log('userId: ', userId);
+    fetch(`https://api.github.com/users/${userId}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then(({ avatar_url, name, location }) => {
+        this.setState({ userName: name, userLocation: location, userAvatar: avatar_url 
+        });
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.userId === prevProps.match.params.userId) return true;
+    return false;
+  }
+
+  render() {
+    // debugger;
+    console.log('state: ', this.state);
+    const { userAvatar, userName, userLocation } = this.state;
+    if (!userAvatar || !userName || !userLocation) {
+      return null;
+    }
+    return (
+      <div className="user">
+        <img 
+        alt="User Avatar" 
+        src={this.state.userAvatar} 
+        className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{this.state.userName}</span>
+          <span className="user__location">{this.state.userLocation}</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default User;
--- LiveSessions/React/Session3_Routers/src/Users.jsx
+++ LiveSessions/React/Session3_Routers/src/Users.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import User from './User.jsx';
 
 const Users = () => {
@@ -15,12 +15,12 @@ const Users = () => {
           <Link to="/users/facebook">Facebook</Link>
         </li>
       </ul>
-      <Route path="/users">
-        <span>Select a user please</span>
-      </Route>
-      <Route>
-        <User path='/users/:userId'/>
-      </Route>
+      <Switch>
+        <Route path="/users/:userId" component={User}/>
+        <Route path="/users">
+          <span>Select a user please</span>
+        </Route>
+      </Switch>
     </div>
   );
 };
